106b3f5a20c0b5f98b3ac07ba303ad499583597a
FreakishLancer <destfinis@gmail.com>
2017-02-02 11:43:25 -0500

    Working iframe preview

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(() => {
+    $("#submit-search").on("click", () => {
+        let searchTerm = $("#search-term").val();
+        console.log(searchTerm);
+    });
+    $.ajax({
+        url: "//en.wikipedia.org/w/api.php",
+        data: {
+            action: "query",
+            list: "search",
+            srsearch: "Hello",
+            format: "json"
+        },
+        dataType: "jsonp",
+        success: x => {
+            console.log("title", x.query.search[0].title);
+        }
+    });
+});
